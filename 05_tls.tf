fd3990078a487681d2dcc8c6393082fb67228890
Siim Aarmaa <siim.aarmaa@hotmail.com>
2021-03-10 18:24:20 +0000

    Delete 05_tls.tf

--- 05_tls.tf
+++
@@ -1,8 +0,0 @@
-data "external" "generated_key_pair" {
-  program = [ "python", "generate_key.py" ]
-}
-
-resource "aws_key_pair" "key2_siimaarmaa" {
-  key_name   = "key2_siimaarmaa"
-  public_key = data.external.generated_key_pair.result.public_key
-}
