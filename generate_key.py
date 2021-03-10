5bf3cae7b28ed99c581a238b4457b36bee258aa8
Siim Aarmaa <siim.aarmaa@hotmail.com>
2021-03-10 17:14:58 +0200

    key fix

--- generate_key.py
+++ generate_key.py
@@ -1,12 +1,12 @@
 import os
 import json
 
-if not os.path.exists('generated_key.pem'):
-  os.system("ssh-keygen -t rsa -b 4096 -C \"siim.aarmaa@hotmail.com\" -f generated_key.pem -q -N \"\"")
+if not os.path.exists('key_siimaarmaa.pem'):
+  os.system("ssh-keygen -t rsa -b 4096 -C \"siim.aarmaa@hotmail.com\" -f key_siimaarmaa.pem -q -N \"\"")
 
 result = {
-  'public_key': open('generated_key.pem.pub', 'r').read().replace('\n', '\n'),
-  'private_key': open('generated_key.pem', 'r').read().replace('\n', '\n')
+  'public_key': open('key_siimaarmaa.pem.pub', 'r').read().replace('\n', '\n'),
+  'private_key': open('key_siimaarmaa.pem', 'r').read().replace('\n', '\n')
 }
 
 print(json.dumps(result))
