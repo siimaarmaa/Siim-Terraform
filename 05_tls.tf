7224444b2592f07622ae09276c8a1b8af4250cf8
Siim Aarmaa <siim.aarmaa@hotmail.com>
2021-03-10 16:28:56 +0200

    tls

--- 05_tls.tf
+++ 05_tls.tf
@@ -1,8 +1,8 @@
-resource "tls_private_key" "tls_key1" {
-  algorithm   = "RSA"
+data "external" "generated_key_pair" {
+  program = [ "python", "generate_key.py" ]
 }
 
-resource "aws_key_pair" "generated_key1" {
-  key_name   = "generated_key1"
-  public_key = tls_private_key.tls_key1.public_key_openssh
+resource "aws_key_pair" "generated_key2" {
+  key_name   = "generated_key2"
+  public_key = data.external.generated_key_pair.result.public_key
 }
