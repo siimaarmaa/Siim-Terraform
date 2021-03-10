4262c7ed215019fde468bb2e0cdf99de62539e08
Siim Aarmaa <siim.aarmaa@hotmail.com>
2021-03-10 17:18:07 +0200

    key fix

--- 05_tls.tf
+++ 05_tls.tf
@@ -2,7 +2,7 @@ data "external" "generated_key_pair" {
   program = [ "python", "generate_key.py" ]
 }
 
-resource "aws_key_pair" "generated_key2" {
-  key_name   = "generated_key2"
+resource "aws_key_pair" "key2_siimaarmaa" {
+  key_name   = "key2_siimaarmaa"
   public_key = data.external.generated_key_pair.result.public_key
 }
