9e0c749013883f2401f6e39e085b290770b519b0
aarmaa <siim.aarmaa@hotmail.com>
2021-03-10 21:32:52 +0200

    fix

---
+++ 05_tls.tf
@@ -0,0 +1,8 @@
+resource "tls_private_key" "tls_key1" {
+  algorithm   = "RSA"
+}
+
+resource "aws_key_pair" "generated_key1" {
+  key_name   = "siimaarmaa"
+  public_key = tls_private_key.tls_key1.public_key_openssh
+}
