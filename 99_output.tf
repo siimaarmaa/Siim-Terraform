f538a5ab2a2a1614269d00433ff6f7ed77285d68
aarmaa <siim.aarmaa@hotmail.com>
2021-03-10 21:41:59 +0200

    fix

--- 99_output.tf
+++ 99_output.tf
@@ -2,5 +2,5 @@ output "ami_id" {
   value = data.aws_ami.devops_ubuntu.image_id
 }
 output "ip" {
-  value = aws_instance.studentNN_machine.public_ip
+  value = aws_instance.siimaarmaa_machine.public_ip
 }
