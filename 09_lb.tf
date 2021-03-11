8713730f4a90504e012d1f01285cc4dc328a9431
aarmaa <siim.aarmaa@hotmail.com>
2021-03-11 12:22:36 +0200

    lb

---
+++ 09_lb.tf
@@ -0,0 +1,34 @@
+resource "aws_lb" "cluster_lb" {
+  name            = "${var.project_name}-lb"
+  internal        = false
+  security_groups = [ data.aws_security_group.workshop_secunity_group.id ]
+  subnets         = [ data.aws_subnet.workshop_subnet_primary.id, data.aws_subnet.workshop_subnet_secondary.id ]
+}
+
+resource "aws_lb_target_group" "cluster_target_group" {
+  name     = "${var.project_name}-elasticsearch"
+  port     = 9200
+  protocol = "HTTP"
+  vpc_id   = data.aws_subnet.workshop_subnet_primary.vpc_id
+}
+
+resource "aws_lb_target_group_attachment" "cluster_target" {
+  count            = var.nodes_count
+  target_group_arn = aws_lb_target_group.cluster_target_group.arn
+  target_id        = aws_instance.cluster_node[count.index].id
+  port             = 9200
+}
+
+resource "aws_lb_listener" "cluster_front_end" {
+  load_balancer_arn = aws_lb.cluster_lb.arn
+  port              = "9200"
+  protocol          = "HTTP"
+  default_action {
+    target_group_arn = aws_lb_target_group.cluster_target_group.arn
+    type             = "forward"
+  }
+}
+
+output "lb_dns" {
+  value = aws_lb.cluster_lb.dns_name
+}
